b0ae35bee3777c2e1fb6a73f2245fa0588caf7e5
Richard Downe <rdowne@figure1.com>
2017-08-22 15:23:58 -0400

    since using double, use here, too

--- src/index.js
+++ src/index.js
@@ -535,7 +535,7 @@ function propertyToPostgres(property, name, schema, isAlter) {
       if (property.decimals && property.decimals > 0) {
         column = 'NUMERIC(' + property.maxLength + ',' + property.decimals + ')';
       } else {
-        column = 'REAL';
+        column = 'DOUBLE PRECISION';
       }
       break;
     case 'object':
