f898a4cdf2b0ac9696afffbb79093e9d070bf33b
Andre Gloria <andredagloria@gmail.com>
2019-04-30 15:36:01 -0300

    Update configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,45 @@
 module.exports = {
-  "extends": "google",
-  "env": {
-    "es6": true
+  extends: 'google',
+  parserOptions: {
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true
+    }
   },
-  "rules": {
-    "generator-star-spacing": [
-      "error", {
-        "before": true,
-        "after": false
-      }],
-    "comma-dangle": 0,
-    "require-jsdoc": 0,
-    "no-extra-parens": 2,
-    "arrow-parens": ["error", "as-needed"],
-    "yield-star-spacing": ["error", {"before": true, "after": false}],
-    "max-len": ["error", {
-      "ignoreComments": true,
-      "ignoreStrings": true,
-      "ignoreTemplateLiterals": true
-    }],
-    "no-var": 0,
-    "default-case": 0
+  env: {
+    node: true,
+    es6: true,
+    mocha: true
+  },
+  rules: {
+    'generator-star-spacing': [
+      'error',
+      {
+        before: true,
+        after: false
+      }
+    ],
+    'comma-dangle': 0,
+    semi: ['error', 'never'],
+    'require-jsdoc': 0,
+    'valid-jsdoc': 0,
+    'new-cap': 0,
+    'quote-props': 0,
+    'no-extra-parens': 0,
+    'arrow-parens': ['error', 'as-needed'],
+    'yield-star-spacing': ['error', {before: true, after: false}],
+    'max-len': [
+      'error',
+      {
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true
+      }
+    ],
+    'no-var': 0,
+    camelcase: 0,
+    'no-invalid-this': 0,
+    'no-undef': 2,
+    'no-unreachable': 2
   }
-};
+}
